821da41c0bdd37169e8918a62e3eca5e036dc29f
veeresh <veeresh@hpe.com>
2021-03-31 22:57:41 +0530

    Add dependecy, fix import

--- example_dags/example_ml.py
+++ example_dags/example_ml.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow.operators.check_operator import CheckOperator, IntervalCheckOperator, ValueCheckOperator
 
@@ -16,13 +16,17 @@ with DAG(
     schedule_interval='0 8 * * *',
     start_date=datetime(2020, 1, 6)
 ) as dag:
-    enter_point = DummyOperator(
-        task_id='enter_point'
+    install_modules_1 = BashOperator(
+        task_id='install_modules_1',
+        bash_command='pip install -U scikit-learn"',
+        dag=dag
     )
+    
     
     train_model = PythonOperator(
         task_id='train_model',
-        python_callable=training
+        python_callable=training,
+        dag = dag
     )
 
 
